bcf408f69ccce2da8f964660df7ab73c44d83e50
MauFGarzo <mauricioflores123321@gmail.com>
2024-10-28 07:21:57 -0400

    Added simple frontend design and integrated Google Maps API for geolocation and permission handling.

--- app/src/main/java/com/example/waldo/MainActivity.kt
+++ app/src/main/java/com/example/waldo/MainActivity.kt
@@ -1,25 +1,40 @@
 package com.example.waldo
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     // Inicializamos después
     private lateinit var map:GoogleMap
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         createFragment()
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     private fun createFragment() {
@@ -29,28 +44,35 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-        createMarker()
-        drawCircle()
+
+        map.uiSettings.isZoomControlsEnabled = true
+        map.setOnMarkerClickListener(this)
+        setUpMap()
     }
 
-    private fun createMarker() {
-        val coordinates = LatLng(-19.039633557431262, -65.25686524810982)
-        val marker = MarkerOptions().position(coordinates).title("Facultad de Tecnología")
-        map.addMarker(marker)
-        map.animateCamera(
-            CameraUpdateFactory.newLatLngZoom(coordinates, 18f),
-            2000,
-            null
-        )
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+        map.isMyLocationEnabled = true
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                placeMarkerOnMap(currentLatLng)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+            }
+        }
     }
-    private fun drawCircle(){
-        val circleOptions = CircleOptions()
-            .center(LatLng(-19.039633557431262, -65.25686524810982))
-            .radius(50.0)
-            .strokeWidth(4f)
-            .strokeColor(0xFF009bff.toInt())
-            .fillColor(0x55bad9ee)
 
-        map.addCircle(circleOptions)
+    private fun placeMarkerOnMap(currentLatLng: LatLng) {
+        val markerOptions = MarkerOptions().position(currentLatLng)
+        markerOptions.title("$currentLatLng")
+        map.addMarker(markerOptions)
     }
+
+    override fun onMarkerClick(p0: Marker) = false
 }
